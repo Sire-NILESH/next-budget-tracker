2b28f22b53ae55a7694cf8801e921feff53515a6
Sire-NILESH <singhnilesh9029@gmail.com>
2024-04-29 17:59:07 +0530

    added shadcn components, implemented setup wizard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProviders";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +26,10 @@ export default function RootLayout({
           colorScheme: "dark",
         }}
       >
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Toaster richColors position="bottom-right" />
+          <RootProviders>{children}</RootProviders>
+        </body>
       </html>
     </ClerkProvider>
   );
